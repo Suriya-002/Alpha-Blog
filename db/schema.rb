3bf8042195e91d0ef13b7c19b55ed64a975d9349
Suriya R <suriyasparks007@gmail.com>
2023-03-29 19:41:01 +0530

    Migration modifications

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_28_064335) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_29_140555) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
