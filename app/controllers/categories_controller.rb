8df863be46c93dfed43809765210bc53934f77dd
Suriya R <suriyasparks007@gmail.com>
2023-03-30 18:39:15 +0530

    Categories creation

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,12 +1,28 @@
 class CategoriesController < ApplicationController
 
   def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "Category was saved successfully"
+      redirect_to @category
+    end
   end
 
   def show
   end
 
   def index
+  end
+
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 
 end
